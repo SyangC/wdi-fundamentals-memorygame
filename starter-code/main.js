3be550679fc3f1eb8ac05485c114b59288b459e5
SyangC <shuyangchia@hotmail.co.uk>
2016-06-17 08:56:59 +0100

    unit 10

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,9 +3,20 @@ var cardTwo = 'Queen';
 var cardThree = 'King';
 var cardFour = 'King';
 
-if (cardOne === cardTwo) {
-	alert('You found a match!')
-}
-else {
-	alert('Sorry, try again.')
+//if (cardOne === cardTwo) {
+//	alert('You found a match!')
+//}
+//else {
+//	alert('Sorry, try again.')
+//}
+
+document.getElementById('game-board');
+
+var createBoard = function() {
+	var x = 4
+	for (var i=0; i<=x; i+=1) {
+		var cardElement = document.createElement('div');
+		carElement.setAttribute = ("card")
+		document.getElementsByTagName('board')[0].appendChild(cardElement);
+	}
 }
